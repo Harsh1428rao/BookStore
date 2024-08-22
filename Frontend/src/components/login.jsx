a67dec762559468b36a8bfda4cb699a3f829a0a0
Harsh Rao <hrao1428@gmail.com>
2024-08-23 00:03:53 +0530

    commit

--- Frontend/src/components/login.jsx
+++ Frontend/src/components/login.jsx
@@ -1,47 +1,67 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-function login() {
+import { useForm } from "react-hook-form"
+
+function Login() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // Add logic to handle form submission, like authentication here
+  };
+
   return (
     <>
-    <div>
-        {/* You can open the modal using document.getElementById('ID').showModal() method */}
-
-<dialog id="my_modal_3" className="modal">
-  <div className="modal-box  dark:bg-slate-900 dark:text-white">
-    <form method="dialog">
-      {/* if there is a button in form, it will close the modal */}
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-    </form>
-    <h3 className="font-bold text-lg">Login!</h3>
-    {/* email */}
-    <div className='mt-4 space-y-2'>
-        <span>
-            Email
-        </span>
-        <br></br>
-        <input type="email" placeholder='Enter your email' className='w-80 px-3 py-1 border rounded-md outline-none'></input>
-    </div>
-    {/* password*/}
-    <div className='mt-4 space-y-2'>
-        <span>
-            Password
-        </span>
-        <br></br>
-        <input type="text" placeholder='Enter your password' className='w-80 px-3 py-1 border rounded-md outline-none'></input>
-    </div>
-    {/*signup*/}
-    <div className='flex justify-around mt-4 '>
-      <button className='bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-150  dark:bg-slate-900 dark:text-white'>
-        Login
-      </button>
-      <p>Not registered? <Link to="/signup" className='underline text-blue-500 cursor-pointer'> SignUp</Link></p>
-      
-    </div>
-  </div>
-</dialog>
-    </div>
+      <div>
+        {/* You can open the modal using document.getElementById('my_modal_3').showModal() method */}
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box dark:bg-slate-900 dark:text-white">
+            <form onSubmit={handleSubmit(onSubmit)} method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
+            </form>
+            <h3 className="font-bold text-lg">Login!</h3>
+            {/* email */}
+            <div className='mt-4 space-y-2'>
+              <span>Email</span>
+              <br />
+              <input 
+                type="email" 
+                placeholder='Enter your email' 
+                className='w-80 px-3 py-1 border rounded-md outline-none'
+                {...register("email", { required: true })}
+              />
+              {errors.email && <span className="text-red-500">Email is required</span>}
+            </div>
+            {/* password */}
+            <div className='mt-4 space-y-2'>
+              <span>Password</span>
+              <br />
+              <input 
+                type="password" 
+                placeholder='Enter your password' 
+                className='w-80 px-3 py-1 border rounded-md outline-none'
+                {...register("password", { required: true })}
+              />
+              {errors.password && <span className="text-red-500">Password is required</span>}
+            </div>
+            {/* login button */}
+            <div className='flex justify-around mt-4'>
+              <button type="submit" className='bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-150 dark:bg-slate-900 dark:text-white'>
+                Login
+              </button>
+              <p>Not registered? <Link to="/signup" className='underline text-blue-500 cursor-pointer'>SignUp</Link></p>
+            </div>
+          </div>
+        </dialog>
+      </div>
     </>
   )
 }
 
-export default login
+export default Login;
