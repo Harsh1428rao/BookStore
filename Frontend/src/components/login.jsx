2a6383c2948888b5779460e0147e730bdabeac12
Harsh Rao <119756737+Harsh1428rao@users.noreply.github.com>
2025-03-12 01:47:40 +0530

    Update login.jsx

--- Frontend/src/components/login.jsx
+++ Frontend/src/components/login.jsx
@@ -16,7 +16,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/login", userInfo)
+      .post("https://bookstore-7-7ubp.onrender.com/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
